23d8b25a957860ffccf27de64747dddb95c8deb0
xihuanbanku <lianghaikun@qq.com>
2018-07-25 14:44:06 +0800

    打开首页后，自动跳转到目标页面

---
+++ slave.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         打开阿凡题
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       lianghaikun@sina.com
+// @match        *://www.zhixue.com/crowdsourcednew/dist*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    window.open("http://www.zhixue.com/crowdsourcednew/api/machining/getReadyCount?_=1532496865332&phaseCode=04&subjectCode=02&taskTypeCode=Proofread&isLimitApply=false&processTypeCode=0");
+})();
