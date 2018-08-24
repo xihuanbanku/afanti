57b4acfd0d2053f2d8e3289259c55b78c9013a4e
xihuanbanku <lianghaikun@qq.com>
2018-08-24 18:24:44 +0800

    因为url里面有#, 不使用@方式排除, 直接用js控制(更灵活)

--- slave.js
+++ slave.js
@@ -4,8 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       lianghaikun@sina.com
-// @match        *://www.zhixue.com/crowdsourcednew/dist/*
-// @exclude      http://www.zhixue.com/crowdsourcednew/dist/assets/ckeditor4.1_math/plugins/ckeditor_wiris/core/editor.html?lang=zh-cn&dir=ltr
+// @match        http://www.zhixue.com/crowdsourcednew/dist/*
 // @grant        none
 // ==/UserScript==
 
@@ -13,5 +12,7 @@
     'use strict';
 
     // Your code here...
-    window.open("http://www.zhixue.com/crowdsourcednew/api/machining/getReadyCount?_=1532496865332&phaseCode=04&subjectCode=02&taskTypeCode=Proofread&isLimitApply=false&processTypeCode=0");
+    if(window.location.href.indexOf("login") >0) {
+        window.open("http://www.zhixue.com/crowdsourcednew/api/machining/getReadyCount?_=1532496865332&phaseCode=04&subjectCode=02&taskTypeCode=Proofread&isLimitApply=false&processTypeCode=0");
+    }
 })();
